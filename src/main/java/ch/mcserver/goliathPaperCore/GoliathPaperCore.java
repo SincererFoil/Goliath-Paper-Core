b8122c9e472064251bfb8bbc99ffb151adf9aa48
SincererFoil <sincererfoilyt@gmail.com>
2026-06-05 23:34:24 +0200

    Initial Commit

---
+++ src/main/java/ch/mcserver/goliathPaperCore/GoliathPaperCore.java
@@ -0,0 +1,16 @@
+package ch.mcserver.goliathPaperCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GoliathPaperCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
